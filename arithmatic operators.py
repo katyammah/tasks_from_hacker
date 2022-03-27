ce02c9841dafcfa77ee5556a0898123490c6099c
katyammah <kat.mahova96@mail.ru>
2022-03-27 22:00:30 +0300

    i change file 'arithmatic operators'

--- arithmatic operators.py
+++ arithmatic operators.py
@@ -2,3 +2,7 @@ if __name__ == '__main__':
     a = int(input())
     b = int(input())
     print(a+b, a-b, a*b, sep='\n')
+
+    #i try change something and i will commit this modification again
+
+    print(a**b)
