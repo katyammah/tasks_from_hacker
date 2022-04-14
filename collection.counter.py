2a2504d27a381137e7df17ce13423025f8ac4884
katyammah <kat.mahova96@mail.ru>
2022-04-14 10:07:53 +0300

    add file 'calendar module'

---
+++ collection.counter.py
@@ -0,0 +1,27 @@
+from collections import Counter
+shoes = int(input())  #кол-во пар обуви
+sizes = map(int, input().split())  #размеры этих пар
+customers = int(input())  #кол-во покупателей
+s = Counter(sizes)
+
+earned = 0
+for i in range(customers):
+    size, price = map(int, input().split())  #нужный покупателю размер и сумма, которую готов заплатить
+    if s[size]:
+        earned += price
+        s[size] -= 1
+
+print(earned)
+
+
+
+
+
+
+
+
+
+
+
+
+
